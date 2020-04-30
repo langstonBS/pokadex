5ea5f6fabd3a39a1b5e61b0b0a837711486936c3
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-29 22:04:07 -0700

    css changese

--- src/App.js
+++ src/App.js
@@ -1,53 +1,107 @@
 import React, { Component } from 'react';
-//import request from 'superagent';
+import request from 'superagent';
 import RenderPokemon from './RenderPokemon';
+import './App.css';
 
 export default class App extends Component {
 
   state = {
-    searchQuery: null,
+    searchQuery: '',
     searchLevle: 0,
-    getData: null,
+    data: [{}],
     attacDefenc: 'attack',
+    ascDesc: 'asc',
+    page: 1,
   }
+
   handleChange = (e) => {
     const value = e.target.value;
     this.setState({ searchQuery: value });
-    console.log(this.state)
-  }
+  };
+
   levelChange = (e) => {
     let value = e.target.value;
-    if (value <= 0|| value - (Math.floor(value)) !== 0) {
+    if (value <= 0 || value - (Math.floor(value)) !== 0) {
       value = 0;
     }
     this.setState({ searchLevle: value });
-    console.log(this.state)
-  }
+  };
+
   sortBy = (e) => {
     const value = e.target.value;
     this.setState({ attacDefenc: value })
-    console.log(value);
-
   }
 
-  handleClick = () => {
-    const getData = 'I am clicked';
-    console.log(getData);
+  asendingDesending = (e) => {
+    const value = e.target.value;
+    this.setState({ ascDesc: value })
+  }
+
+  handleClick = async () => {
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}&perPage=24&sort=${this.state.attacDefenc}&direction=${this.state.ascDesc}`);
+    this.setState({ data: fetchedData.body.results });
+  }
+
+  pagePlacePrevus = async () => {
+    let curentPage = this.state.page;
+    let Newpage
+    if (curentPage === 1) {
+      Newpage = curentPage;
+    } else {
+      Newpage = curentPage - 1;
+    }
+    this.setState({ page: Newpage });
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}&perPage=24&sort=${this.state.attacDefenc}&direction=${this.state.ascDesc}&page=${this.state.page}`);
+    this.setState({ data: fetchedData.body.results });
+  }
+  
+  pagePlaceNext = async () => {
+    let curentPage = this.state.page;
+    let Newpage = curentPage + 1;
+    this.setState({ page: Newpage });
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}&perPage=24&sort=${this.state.attacDefenc}&direction=${this.state.ascDesc}&page=${this.state.page}`);
+    this.setState({ data: fetchedData.body.results });
+
 }
+
+
 
   render() {
     return (
-      <div>
-        i am here
-        <input onChange={this.handleChange} />
-        <input type="number" pattern="[0-9]" onChange={this.levelChange} />
-        <select className="selctionStyle" onChange={this.sortBy}>
-          <option value="attack">attack</option>
-          <option value="defend">defend</option>
-        </select>
-        < button onClick={this.handleClick}>serch</button>
-        < RenderPokemon />
-      </div>
+      <>
+        <div className="searchList">
+            <span className="nameSerch">
+              <h3>name</h3>
+              <input onChange={this.handleChange} />
+          </span>
+          <div>
+              <h3>Defence</h3>
+            <input type="number" pattern="[0-9]" onChange={this.levelChange} />
+            </div>
+            <span className="sortby">
+              <h3>Order by</h3>
+              <select className="selctionStyle" onChange={this.sortBy}>
+                <option value="attack">attack</option>
+                <option value="defense">defense</option>
+              </select>
+              <select className="selctionStyle" onChange={this.asendingDesending}>
+                <option value="asc">Week</option>
+                <option value="desc">Strong</option>
+              </select>
+          </span>
+          </div>
+            <span className="search">
+          < button onClick={this.pagePlacePrevus}>Prevus</button>
+          < button onClick={this.handleClick}>Search</button>
+          < button onClick={this.pagePlaceNext}>Next</button>
+          </span>
+          
+          <span className="renderList">  
+        <ul>
+          {this.state.data.map(item => <RenderPokemon pokemon={item} />)}
+        </ul>
+        </span>
+      </>
     )
   }
 }
--- src/RenderPokemon.js
+++ src/RenderPokemon.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import './RenderPokemon.css';
 
 export default class RenderPokemon extends Component {
     render() {
         return (
-            <div>
-                <h1>this is me </h1>
-            </div>
+            <li>
+                <div className="immageContainer">
+                    <h2>{this.props.pokemon.pokemon}</h2>
+                    <p>Pokemon type: {this.props.pokemon.type_1}</p>
+                    <p>Attack levle: {this.props.pokemon.attack}</p>
+                    <p>Defense levle: {this.props.pokemon.defense}</p>
+                    <div className="immageContatin">
+                        <img alt={this.props.pokemon.pokemon}
+                        src={this.props.pokemon.url_image}></img>
+                    </div>
+                    
+                </div>
+            </li>
         )
     }
 }
