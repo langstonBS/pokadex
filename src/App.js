0b44bd83cdab1962475e9710de82c6c940a4a3d9
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-29 17:00:33 -0700

    created the serch funtion started on the render page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import request from 'superagent';
+import RenderPokemon from './RenderPokemon';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    searchQuery: null,
+    searchLevle: 0,
+    getData: null,
+    attacDefenc: 'attack',
+  }
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({ searchQuery: value });
+    console.log(this.state)
+  }
+  levelChange = (e) => {
+    let value = e.target.value;
+    if (value <= 0|| value - (Math.floor(value)) !== 0) {
+      value = 0;
+    }
+    this.setState({ searchLevle: value });
+    console.log(this.state)
+  }
+  sortBy = (e) => {
+    const value = e.target.value;
+    this.setState({ attacDefenc: value })
+    console.log(value);
+
+  }
+
+  handleClick = () => {
+    const getData = 'I am clicked';
+    console.log(getData);
 }
 
-export default App;
+  render() {
+    return (
+      <div>
+        i am here
+        <input onChange={this.handleChange} />
+        <input type="number" pattern="[0-9]" onChange={this.levelChange} />
+        <select className="selctionStyle" onChange={this.sortBy}>
+          <option value="attack">attack</option>
+          <option value="defend">defend</option>
+        </select>
+        < button onClick={this.handleClick}>serch</button>
+        < RenderPokemon />
+      </div>
+    )
+  }
+}
---
+++ src/RenderPokemon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RenderPokemon extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is me </h1>
+            </div>
+        )
+    }
+}
